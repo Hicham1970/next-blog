1cb8dec6d7eabc8a2c7b885ed824c2f9f40ffa7c
Hicham1970 <devhicham2@gmail.com>
2025-06-01 21:23:07 +0100

    changer le webhooks/route.js

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,7 +1,7 @@
 import { Webhook } from 'svix';
 import { headers } from 'next/headers';
-import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
-import { clerkClient } from '@clerk/nextjs/server';
+// import { createOrUpdateUser, deleteUser } from '@/lib/actions/user';
+// import { clerkClient } from '@clerk/nextjs/server';
 
 export async function POST(req) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
@@ -57,47 +57,11 @@ export async function POST(req) {
     console.log('Webhook body:', body);
 
     if (eventType === 'user.created' || eventType === 'user.updated') {
-        const { id, first_name, last_name, image_url, email_addresses, username } =
-            evt?.data;
-        try {
-            const user = await createOrUpdateUser(
-                id,
-                first_name,
-                last_name,
-                image_url,
-                email_addresses,
-                username
-            );
-            if (user && eventType === 'user.created') {
-                try {
-                    await clerkClient.users.updateUserMetadata(id, {
-                        publicMetadata: {
-                            userMongoId: user._id,
-                            isAdmin: user.isAdmin,
-                        },
-                    });
-                } catch (error) {
-                    console.log('Error updating user metadata:', error);
-                }
-            }
-        } catch (error) {
-            console.log('Error creating or updating user:', error);
-            return new Response('Error occured', {
-                status: 400,
-            });
-        }
+        console.log('User created or updated:', evt.data);
     }
 
     if (eventType === 'user.deleted') {
-        const { id } = evt?.data;
-        try {
-            await deleteUser(id);
-        } catch (error) {
-            console.log('Error deleting user:', error);
-            return new Response('Error occured', {
-                status: 400,
-            });
-        }
+        console.log('User deleted:', evt.data);
     }
 
     return new Response('', { status: 200 });
