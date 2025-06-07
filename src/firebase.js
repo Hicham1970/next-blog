ab1572cd3e0bd1aecb9e251a58865d9497167d8d
Hicham1970 <devhicham2@gmail.com>
2025-06-07 01:21:53 +0100

    Creation d'un nouveau projet Firebase et creation du fichier src/firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDXknQ50Y-_4TzDmecyTplc-BteHgcZrcs",
+    authDomain: "next-blog-c6bf8.firebaseapp.com",
+    projectId: "next-blog-c6bf8",
+    storageBucket: "next-blog-c6bf8.firebasestorage.app",
+    messagingSenderId: "7828039539",
+    appId: "1:7828039539:web:0e989f07999a0e25d04706"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
