585a2ed475acff0edf59062be882902e216f9699
Hicham1970 <devhicham2@gmail.com>
2025-06-13 14:26:12 +0100

    First commit

--- src/app/sign-in/[[...sign-in]]/page.jsx
+++ src/app/sign-in/[[...sign-in]]/page.jsx
@@ -9,14 +9,6 @@ export default function SignInPage() {
   return (
     <div className="flex items-center justify-center text-center font-semibold text-2xl  text-red-500">
       <SignIn
-        appearance={{
-          elements: {
-            rootBox: "mx-2 md:mx-0 max-w-md w-full",
-            formFieldInput:
-              "bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-700 focus:border-blue-500 focus:ring-blue-500",
-            card: "shadow-xl",
-          },
-        }}
       />
 
       {isSignedIn && user.publicMetadata.isAdmin && (
