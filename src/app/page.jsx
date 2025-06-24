fa33e4f49b32319ddda2c7bef4090455dccebd55
Hicham1970 <devhicham2@gmail.com>
2025-06-24 22:15:39 +0100

    Home page Completed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <>
-    <h1>
-      The home page
-      </h1>
-      <p>Les applications ne sont pas comme ce que tout la monde croit, non, se sont des bouffeuses d'énergie.ra</p>
-      </>
-  )
+    <div className="container flex flex-col md:flex-row gap-5 h-[calc(100vh-4rem)]">
+      <div className="basis-full flex flex-col justify-center md:basis-2/3">
+        <p className="special-word text-xs">Protect All the Birds</p>
+        <h1 className="pb-5">
+          The World's <span className="special-word ">Rarest</span><br /> Birds
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quidem molestias.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quidem molestias.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, quidem molestias.  
+        </p>
+      </div>
+      <div className="md:block basis-1/3 mt-12">
+        <Image
+          src="/img/ara3.png"
+          alt="Ara"
+          width={500}
+          height={500}
+          className="object-cover h-full w-full md:h-[500px] md:w-[500px]"
+        
+        />
+      </div>
+    </div>
+  );
 }
 
  
